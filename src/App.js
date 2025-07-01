c9293803f2baa8a06bad0bc66cf84da0a8b1e99d
jungchang0206 <jungchang02062004@gmail.com>
2025-07-01 19:31:13 -0400

    Added background

--- src/App.js
+++ src/App.js
@@ -7,21 +7,42 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import LetterGlitch from './components/LetterGlitch';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <About />
-        <Education />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
+      {/* Background Component */}
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -1
+      }}>
+        <LetterGlitch
+          glitchColors={['#2b4539', '#61dca3', '#61b3dc']}
+          glitchSpeed={50}
+          smooth={true}
+        />
+      </div>
+
+      {/* Content */}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Header />
+        <main>
+          <About />
+          <Education />
+          <Skills />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
